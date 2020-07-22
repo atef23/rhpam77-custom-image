ffa03e9ef83ec4e092cf6b98da1a3330c6c7cfbb
atef23 <atefphone@gmail.com>
2020-07-22 10:22:05 -0400

    Update standalone-update-logging.sh

--- standalone-update-logging.sh
+++ standalone-update-logging.sh
@@ -2,28 +2,28 @@
 
 xmlstarlet ed --inplace -N logging-namespace="urn:jboss:domain:logging:6.0" \
   -s /_:server/_:profile/logging-namespace:subsystem -t elem -n file-handler -v "" \
-  standalone-openshift.xml
+  /opt/eap/standalone/configuration/standalone-openshift.xml
 
 xmlstarlet ed --inplace -N logging-namespace="urn:jboss:domain:logging:6.0" \
   -i "/_:server/_:profile/logging-namespace:subsystem/logging-namespace:file-handler[not(@name)]" -t 'attr' -n name -v "FILE" \
-  standalone-openshift.xml
+  /opt/eap/standalone/configuration/standalone-openshift.xml
 
 xmlstarlet ed --inplace -N logging-namespace="urn:jboss:domain:logging:6.0" \
   -s "/_:server/_:profile/logging-namespace:subsystem/logging-namespace:file-handler[@name='FILE']" -t elem -n formatter -v "" \
-  standalone-openshift.xml
+  /opt/eap/standalone/configuration/standalone-openshift.xml
 
 xmlstarlet ed --inplace -N logging-namespace="urn:jboss:domain:logging:6.0" \
   -s "/_:server/_:profile/logging-namespace:subsystem/logging-namespace:file-handler[@name='FILE']/logging-namespace:formatter" -t elem -n named-formatter -v "" \
-  standalone-openshift.xml
+  /opt/eap/standalone/configuration/standalone-openshift.xml
 
 xmlstarlet ed --inplace -N ln="urn:jboss:domain:logging:6.0" \
   -i "/_:server/_:profile/ln:subsystem/ln:file-handler[@name='FILE']/ln:formatter/ln:named-formatter" -t 'attr' -n name -v "COLOR-PATTERN" \
-  standalone-openshift.xml
+  /opt/eap/standalone/configuration/standalone-openshift.xml
 
 xmlstarlet ed --inplace -N ln="urn:jboss:domain:logging:6.0" \
   -s "/_:server/_:profile/ln:subsystem/ln:root-logger/ln:handlers" -t 'elem' -n handler -v "" \
-  standalone-openshift.xml
+  /opt/eap/standalone/configuration/standalone-openshift.xml
 
 xmlstarlet ed --inplace -N ln="urn:jboss:domain:logging:6.0" \
   -s "/_:server/_:profile/ln:subsystem/ln:root-logger/ln:handlers/ln:handler[not(@name)]" -t 'attr' -n name -v "FILE" \
-  standalone-openshift.xml
+  /opt/eap/standalone/configuration/standalone-openshift.xml
